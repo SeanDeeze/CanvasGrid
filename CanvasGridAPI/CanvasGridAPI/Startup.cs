57edc1597338e8d36b0ebc4fd64d8c8fd1db9c3f
SeanDeeze <davidseandunn@gmail.com>
2020-06-23 18:31:23 -0700

    Allow Any origin for live site

--- CanvasGridAPI/CanvasGridAPI/Startup.cs
+++ CanvasGridAPI/CanvasGridAPI/Startup.cs
@@ -29,7 +29,7 @@ namespace CanvasGridAPI
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
-                    builder => builder.WithOrigins("http://104.168.143.158/")
+                    builder => builder.AllowAnyOrigin()
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials());
