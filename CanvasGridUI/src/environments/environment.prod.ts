305220d365db01288f0b89dde2e11ec87ab17178
SeanDeeze <davidseandunn@gmail.com>
2020-06-23 21:42:05 -0700

    added slash to ensure all api calls work

--- CanvasGridUI/src/environments/environment.prod.ts
+++ CanvasGridUI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serviceURL: 'http://104.168.143.158/CanvasGrid',
+  serviceURL: 'http://104.168.143.158/CanvasGrid/',
   imagesURL: 'http://104.168.143.158/CanvasGrid/Images/'
 };
