a52940e64ad0de3241776e767b5fb7fddef781a1
Sean Dunn <davidseandunn@gmail.com>
2026-01-18 18:16:10 -0500

    upgrade to angular version 18

--- CanvasGridAPI/CanvasGridAPI/CanvasGridUI/src/app/app.module.ts
+++ CanvasGridAPI/CanvasGridAPI/CanvasGridUI/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { GridService } from './services/grid.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    GridComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [GridService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        GridComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [GridService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
